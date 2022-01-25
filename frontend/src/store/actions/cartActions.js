9729580cae44c189093bcecb999ab43f0ff81202
novovicn <novovicnenad96@gmail.com>
2022-01-25 22:10:58 +0100

    trial ssh

--- frontend/src/store/actions/cartActions.js
+++ frontend/src/store/actions/cartActions.js
@@ -1,37 +1,33 @@
 import axios from 'axios';
 
-
 export const addToCart = (id, qty) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/products/${id}`);
 
-    const { data } = await axios.get(`/api/products/${id}`);
+  dispatch({
+    type: 'ADD_ITEM',
+    payload: {
+      _id: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      countInStock: data.countInStock,
+      qty,
+    },
+  });
 
-    dispatch({
-        type: 'ADD_ITEM',
-        payload: {
-            _id: data._id,
-            name: data.name,
-            image: data.image,
-            price: data.price,
-            countInStock: data.countInStock,
-            qty
-        }
-    })
-
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
 
 export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: 'REMOVE_ITEM',
+    payload: id,
+  });
 
-    dispatch({
-        type: 'REMOVE_ITEM',
-        payload: id
-    })
-
-    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
-}
-
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
 
 export const saveShippingAddress = (data) => async (dispatch) => {
-    dispatch({ type: 'CART_SAVE_SHIPPING_ADDRESS', payload: data });
-    localStorage.setItem('shippingAddress', JSON.stringify(data));
-  };
+  dispatch({ type: 'CART_SAVE_SHIPPING_ADDRESS', payload: data });
+  localStorage.setItem('shippingAddress', JSON.stringify(data));
+};
