31079c71a40a753803912a8c29cb684849c9e18c
novovicn <novovicnenad96@gmail.com>
2022-02-12 19:09:37 +0100

    add a product review backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -79,4 +79,37 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 })
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct };
+const createProductReview = asyncHandler(async (req, res) => {
+
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id)
+
+  if (product) {
+
+    const alreadyReviewed = product.reviews.find(review => review.user.toString() === req.user._id.toString());
+
+    if(alreadyReviewed){
+      throw new Error('Product already reviewed!');
+    }
+    
+    const review = {
+      rating: Number(rating),
+      name: req.user.name,
+      comment,
+      user: req.user._id
+    }
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating = product.reviews.reduce((acc, item) => acc + item.rating, 0) / product.reviews.length;
+
+    const updatedProduct = await product.save()
+    res.json(updatedProduct)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProductById, getProducts, deleteProduct, createProduct, updateProduct } from '../controllers/productController.js';
+import { getProductById, getProducts, deleteProduct, createProduct, updateProduct, createProductReview } from '../controllers/productController.js';
 import { isAdmin, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, isAdmin, createProduct)
 router.route('/:id').get(getProductById).delete(protect, isAdmin, deleteProduct).put(protect, isAdmin, updateProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
