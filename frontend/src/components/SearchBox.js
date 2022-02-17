dd6a7c57bfa999b9bc9f82a126d93361f787087d
novovicn <novovicnenad96@gmail.com>
2022-02-17 19:30:51 +0100

    minor styling corrections

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -15,7 +15,7 @@ const SearchBox = ({history}) => {
     }
 
     return (
-    <Form onSubmit={submitHandler} inline="true">
+    <Form onSubmit={submitHandler} className='d-flex'>
       <Form.Control
         type="text"
         name="q"
@@ -23,7 +23,7 @@ const SearchBox = ({history}) => {
         placeholder="Search products.."
         className="mr-sm-2 ml-sm-5"
       ></Form.Control>
-      <Button type='submit' variant='outline-success' className='p-2'>
+      <Button type='submit' variant='outline-info' className='p-2 rounded'>
           Search
       </Button>
     </Form>
